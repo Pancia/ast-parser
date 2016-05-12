67b6648e8599b001f56c80cbec2b0ad98d603259
Anthony D'Ambrosio <adambrosio@thenavisway.com>
2016-05-12 14:00:14 -0700

    Upgrading clojure(script) & co

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject synfnetic "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.228"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.8.51"]
                  [org.omcljs/om "1.0.0-alpha32" :scope "provided"]
                  [navis/untangled-spec "0.3.6" :scope "test"]]
 
@@ -24,9 +24,9 @@
                                        :optimizations        :none}}]}
 
   :profiles {:dev {:source-paths ["dev" "src"]
-                   :dependencies [[binaryage/devtools "0.5.2"]
+                   :dependencies [[binaryage/devtools "0.6.1"]
                                   [com.cemerick/piggieback "0.2.1"]
-                                  [figwheel-sidecar "0.5.0-6" :exclusions [joda-time clj-time]]
+                                  [figwheel-sidecar "0.5.3-1" :exclusions [joda-time clj-time]]
                                   [org.clojure/tools.nrepl "0.2.12"]]
                    :repl-options {:init-ns user
                                   :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}})
